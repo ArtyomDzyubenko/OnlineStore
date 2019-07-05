0f4565cbac74bc37c1f4bb1b95273dde78e2712c
Artyom <artyom.dz@gmail.com>
2019-07-05 23:46:28 +0600

    Add project files.

---
+++ OnlineStore/Controllers/BasketController.cs
@@ -0,0 +1,133 @@
+﻿using OnlineStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineStore.Controllers
+{
+    public class BasketController : Controller
+    {
+        private readonly static String BASKET_NAME = "basket";
+        private online_storeEntities db = new online_storeEntities();
+
+        // GET: basket
+        public ActionResult Index()
+        {
+            return View(GetGoodsFromCookies());
+        }
+        
+        public ActionResult AddToBasket(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Goods goods = db.Goods.Find(id);
+
+            if (goods == null)
+            {
+                return HttpNotFound();
+            }
+
+            HttpCookie basket = Request.Cookies[BASKET_NAME];
+
+            if (basket == null)
+            {
+                basket = new HttpCookie(BASKET_NAME);
+            }
+
+            String goodsId = "g_" + goods.goods_id;
+
+            if (!basket.Values.AllKeys.Contains(goodsId))
+            {
+                basket.Values.Add(goodsId, id.ToString());
+            }
+
+            Response.Cookies.Set(basket);
+
+            return Redirect("/Goods/Index");
+        }
+
+        public ActionResult DeleteFromBasket(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            String goodsId = "g_" + id;
+
+            if (!Request.Cookies[BASKET_NAME].Values.AllKeys.Contains(goodsId))
+            {
+                return HttpNotFound();
+            }
+
+            NameValueCollection goods = Request.Cookies[BASKET_NAME].Values;
+            goods.Remove(goodsId);
+
+            HttpCookie basket = new HttpCookie(BASKET_NAME);
+            basket.Values.Add(goods);
+
+            Response.Cookies.Set(basket);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteAllFromBasket()
+        {
+            Response.Cookies.Set(new HttpCookie(BASKET_NAME));
+
+            return RedirectToAction("Index");
+        }
+
+        protected List<Goods> GetGoodsFromCookies()
+        {
+            List<Goods> goodsList = new List<Goods> { };
+
+            HttpCookie basket = Request.Cookies[BASKET_NAME];
+
+            if (basket == null)
+            {
+                Response.Cookies.Add(new HttpCookie(BASKET_NAME));
+                return goodsList;
+            }
+
+            foreach (String key in basket.Values.AllKeys)
+            {
+                if (key == null)
+                {
+                    continue;
+                }
+
+                if (!Int64.TryParse(basket.Values.Get(key), out long id))
+                {
+                    return goodsList;
+                }
+
+                Goods goods = db.Goods.Find(id);
+
+                if (goods != null)
+                {
+                    goodsList.Add(goods);
+                }
+            }
+
+            return goodsList;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ OnlineStore/Controllers/GoodsController.cs
@@ -0,0 +1,157 @@
+﻿using OnlineStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OnlineStore.Util;
+
+namespace OnlineStore.Controllers
+{
+    public class GoodsController : Controller
+    {
+        private online_storeEntities db = new online_storeEntities();
+
+        // GET: Goods
+        public ActionResult Index()
+        {
+            return View(db.Goods.ToList());
+        }
+
+        // GET: Goods/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Goods goods = db.Goods.Find(id);
+
+            if (goods == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(goods);
+        }
+
+        // GET: Goods/Create
+        [Authorize(Roles ="ORA_DBA")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Goods/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles ="ORA_DBA")]
+        public ActionResult Create([Bind(Include = "goods_id,name,description,price")] Goods goods)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Goods.Add(goods);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(goods);
+        }
+
+        // GET: Goods/Edit/5
+        [Authorize(Roles = "ORA_DBA")]
+        public ActionResult Edit(long? id)
+        {
+            if (!UserRoleChecker.IsUserAdmin())
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+            }
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Goods goods = db.Goods.Find(id);
+
+            if (goods == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(goods);
+        }
+
+        // POST: Goods/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "ORA_DBA")]
+        public ActionResult Edit([Bind(Include = "goods_id,name,description,price")] Goods goods)
+        {
+            if (!UserRoleChecker.IsUserAdmin())
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+            }
+
+            if (ModelState.IsValid)
+            {
+                db.Entry(goods).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(goods);
+        }
+
+        // GET: Goods/Delete/5
+        [Authorize(Roles = "ORA_DBA")]
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Goods goods = db.Goods.Find(id);
+
+            if (goods == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(goods);
+        }
+
+        // POST: Goods/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "ORA_DBA")]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            Goods goods = db.Goods.Find(id);
+            db.Goods.Remove(goods);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+    }
+}
